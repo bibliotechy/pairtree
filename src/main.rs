3d23c01e9a53ba4aa6e295c23f0a84f03e8ba532
Chad Nelson <chadbnelson@gmail.com>
2021-05-13 17:13:24 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,162 @@
+use structopt::{StructOpt};
+use walkdir::{WalkDir,DirEntry};
+use std::fs::read;
+use std::io;
+use std::str::FromStr;
+use std::num::ParseIntError;
+use md5::{Md5, Digest};
+use sha1::{Sha1};
+use sha1::Digest as Sha1Digest;
+
+#[derive(Clone, Copy)]
+enum HashType{
+    MD5,
+    SHA1,
+    Blake3,
+}
+
+impl FromStr for HashType {
+    type Err = ParseIntError;
+    fn from_str(hash_type: &str) -> Result<Self, Self::Err> {
+        match hash_type {
+            "md5" => Ok(HashType::MD5),
+            "sha1" => Ok(HashType::SHA1),
+            "blake3" => Ok(HashType::Blake3),
+            _ => panic!("Panic") // Want this to be an error, but giving me weird warnings
+        }
+    }
+}
+
+
+
+
+#[derive(StructOpt)]
+#[structopt(rename_all = "kebab-case")]
+struct Cli {
+    
+    #[structopt(short = "s", long, parse(from_os_str))]
+    source_dir: std::path::PathBuf,
+
+    #[structopt(short = "d", long, parse(from_os_str))]
+    dest_dir: std::path::PathBuf,
+
+    #[structopt(short = "h", long, default_value = "blake3")]
+    hash_type: HashType,
+
+    #[structopt(short = "u", long)]
+    use_hash_for_filename: bool,
+
+    #[structopt(short = "k", long)]
+    keep_file_extension: bool,
+
+    #[structopt(short = "o", long)]
+    origin_path_in_dest_name: bool
+
+}
+
+
+struct Pairtree {
+    entry: DirEntry,
+    dest_dir: String,
+    hash_type: HashType, 
+    use_hash_for_filename: bool,
+    keep_file_extension: bool,
+    origin_path_in_dest_name: bool
+}
+
+impl Pairtree {
+    fn hex_string(&self) -> String{
+        let f = read(self.entry.path()).expect("no file found").to_owned();
+        let hex = match self.hash_type {
+            MD5    => self.md5_hash(&f),
+            Blake3 => blake3::hash(f.as_ref()).to_hex().as_str().to_owned(),
+            SHA1   => self.sha1_hash(&f)
+        };
+        return hex
+    }
+
+    fn dest_path_base(&self) -> std::path::PathBuf{
+        let out_path: std::path::PathBuf = [
+            &self.dest_dir, 
+            &self.hex_string()[0..2].to_string(), 
+            &self.hex_string()[2..4].to_string()
+        ].iter().collect();
+        return out_path
+    }
+
+    fn dest_path(&self) -> std::path::PathBuf {
+        let mut out_path = self.dest_path_base();
+        if !self.use_hash_for_filename {
+            let filename = self.entry.file_name().to_str().unwrap();
+            out_path.push(filename);
+        }
+        else {
+            let filename = self.hex_string();
+
+            if self.keep_file_extension {
+                let ex = self.entry.path().extension().unwrap().to_str().unwrap();
+                let mut owned_filename = filename.to_owned();
+                owned_filename.push_str(ex);
+                out_path.push(owned_filename);
+            }
+            else {
+                out_path.push(filename)
+            }
+        }
+        
+        return out_path 
+    }
+
+    fn md5_hash(&self, file_contents: &Vec<u8>) -> String {
+        let mut hasher = Md5::new();
+        hasher.update(file_contents);
+        let result = hasher.finalize();
+        let res = std::str::from_utf8(&result).unwrap();
+        return res.to_owned()
+    }
+
+    fn sha1_hash(&self, file_contents: &Vec<u8>) -> String {
+        let mut hasher = Sha1::new();
+        hasher.update(file_contents);
+        let result = hasher.finalize();
+        let res = std::str::from_utf8(&result).unwrap(); 
+        return res.to_owned()
+    }
+}
+
+
+fn main() -> io::Result<()> {
+    let args = Cli::from_args();
+
+    let dest_dir = match args.dest_dir.to_str() {
+        Some(x) => x,
+        None    => panic!("Dest dir not specified")
+    };
+
+    if !args.dest_dir.exists() {
+        std::fs::create_dir_all(args.dest_dir.as_path())?;
+    }
+   
+
+    for entry in WalkDir::new(args.source_dir).into_iter().filter_map(|e| e.ok()) {
+        
+        // We can skip directories
+        if entry.file_type().is_file() {
+
+            let pt = Pairtree{
+                entry: entry, 
+                dest_dir: String::from(dest_dir),
+                hash_type: args.hash_type,
+                use_hash_for_filename:  args.use_hash_for_filename, 
+                keep_file_extension:  args.keep_file_extension, 
+                origin_path_in_dest_name: args.origin_path_in_dest_name
+            };
+            
+            // Ensure the pairtree directory exists
+            std::fs::create_dir_all(pt.dest_path_base())?;
+            std::fs::rename(pt.entry.path(), pt.dest_path())?;
+        }
+    }
+    Ok(())
+    
+}
